ecaf63173b53123e4c2ab710f8109ccc7bb18a4b
taekkim <taekkim@student.42seoul.kr>
2020-12-10 16:46:43 +0900

    unset identifer err msg

--- srcs/unset.c
+++ srcs/unset.c
@@ -12,6 +12,34 @@
 
 #include "../inc/minishell.h"
 
+int		is_identifier(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] > 21 && str[i] < 48) || \
+			(str[i] > 57 && str[i] < 65) || \
+			(str[i] > 90 && str[i] < 97) || \
+			(str[i] > 122 && str[i] < 127))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int 	write_unset_error(char *str, char *err, t_all *a, int exit)
+{
+	ft_putstr_fd("bash: unset: `", 2);
+	ft_putstr_fd(str, 2);
+	ft_putchar_fd('\'', 2);
+	ft_putendl_fd(err, 2);
+	g_end = exit;
+	a->cd = 0;
+	return (g_end);
+}
+
 void 	delete_env(char **mat, t_all *a)
 {
 	int i;
@@ -28,8 +56,9 @@ void 	delete_env(char **mat, t_all *a)
 				a->env = ft_delete_row(a->env, j);
 				break ;
 			}
-
 		}
+		if (!a->env[j] && is_identifier(mat[i]))
+			write_unset_error(mat[i], ": not a valid identifier", a, 1);
 	}
 }
 
