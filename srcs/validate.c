deb54131e971f245e4f36c809a41c0192a4abbf8
nonalias <ktw1532@naver.com>
2020-12-11 18:16:23 +0900

    norm: validate.c #1

--- srcs/validate.c
+++ srcs/validate.c
@@ -1,16 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taehkim <taehkim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/11 18:10:58 by taehkim           #+#    #+#             */
+/*   Updated: 2020/12/11 18:10:59 by taehkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/minishell.h"
 
-int		validate(t_all *a)
+char	*validate2(t_all *a, char *message)
 {
 	char	*temp;
-	char	*message;
 	int		i;
 
-	if (!a->cmd)
-		return (0);
-	if (!(is_sep_char(a->cmd[0]) || is_pipe_or_scolon(a->cmd[0])))
-		return (1);
-	message = ft_strdup("bash: syntax error near unexpected token `");
 	if (is_pipe_or_scolon(a->cmd[0]))
 		message = ft_strcjoin(message, a->cmd[0]);
 	else if (a->cmd && a->arg)
@@ -28,6 +34,19 @@ int		validate(t_all *a)
 		message = ft_strjoin(message, "newline");
 		free(temp);
 	}
+	return (message);
+}
+
+int		validate(t_all *a)
+{
+	char	*message;
+
+	if (!a->cmd)
+		return (0);
+	if (!(is_sep_char(a->cmd[0]) || is_pipe_or_scolon(a->cmd[0])))
+		return (1);
+	message = ft_strdup("bash: syntax error near unexpected token `");
+	message = validate2(a, message);
 	message = ft_strcjoin(message, '\'');
 	ft_putendl_fd(message, 1);
 	free(message);
