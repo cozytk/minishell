236e11f446bbe977af660726387e9da3d99b335a
nonalias <ktw1532@naver.com>
2020-12-11 18:37:52 +0900

    norm: equal.c #1

--- srcs/equal.c
+++ srcs/equal.c
@@ -1,13 +1,28 @@
 #include "../inc/minishell.h"
 
-void	equal_process2(t_all *a, char *env)
+char	*equal_process2(t_all *a, char *env)
 {
 	int		i;
 
-	i = 0;
-	if ((i = find_row(a->sub_env, env)) != -1)
-		a->sub_env = ft_delete_row(a->sub_env, i);
-	a->sub_env = add_row(a->sub_env, a->p.candidate);
+	if (((i = find_exact_row(a->env, env)) != -1) ||
+			((env = ft_strcjoin(env, a->p.candidate[i]))
+				&& (i = find_row(a->env, env) != -1)))
+	{
+		a->cmd = ft_strdup("export");
+		add_argument(a, a->p.candidate);
+		export(a);
+		free(a->cmd);
+		a->cmd = NULL;
+		ft_free_mat(a->arg);
+		a->arg = NULL;
+	}
+	else
+	{
+		if ((i = find_row(a->sub_env, env)) != -1)
+			a->sub_env = ft_delete_row(a->sub_env, i);
+		a->sub_env = add_row(a->sub_env, a->p.candidate);
+	}
+	return (env);
 }
 
 void	equal_process(t_all *a)
@@ -22,20 +37,10 @@ void	equal_process(t_all *a)
 		env = ft_strcjoin(env, a->p.candidate[i]);
 		i++;
 	}
-	if (((i = find_exact_row(a->env, env)) != -1) ||
-			((env = ft_strcjoin(env, a->p.candidate[i]))
-				&& (i = find_row(a->env, env) != -1)))
-	{
-		a->cmd = ft_strdup("export");
-		add_argument(a, a->p.candidate);
-		export(a);
-		free(a->cmd);
-		a->cmd = NULL;
-		ft_free_mat(a->arg);
-		a->arg = NULL;
-	}
-	else
-		equal_process2(a, env);
+	env = equal_process2(a, env);
 	if (env)
+	{
 		free(env);
+		env = NULL;
+	}
 }
