bdf6972dc70267181f3b52169d7e5c7456f41e0b
nonalias <ktw1532@naver.com>
2020-12-11 20:27:53 +0900

    norm: write.c #2

--- srcs/write.c
+++ srcs/write.c
@@ -2,7 +2,7 @@
 
 void	write_quote(char *str)
 {
-	int i;
+	int		i;
 
 	i = -1;
 	while (str[++i] && str[i] != '=')
@@ -17,6 +17,7 @@ void	write_quote(char *str)
 		ft_putchar_fd(str[i], 1);
 	ft_write("\"\n");
 }
+
 int		write_error(char *cmd, char *str, char *err, int exit)
 {
 	ft_putstr_fd("bash: ", 2);
