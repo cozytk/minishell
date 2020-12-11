a04d518615957fc5c961e92c24482dc3922d0440
nonalias <ktw1532@naver.com>
2020-12-11 18:07:17 +0900

    norm: show.c

--- srcs/show.c
+++ srcs/show.c
@@ -2,7 +2,7 @@
 
 void	show_com(t_all *a)
 {
-    write(1, &"==========command==========\n", 28);
+	write(1, &"==========command==========\n", 28);
 	if (!a->cmd)
 		return ;
 	write(1, &"[", 1);
@@ -18,7 +18,7 @@ void	show_arg(t_all *a)
 	i = 0;
 	if (!a->arg)
 		return ;
-    write(1, &"!!!!!!!!!!argument!!!!!!!!!!\n", 29);
+	write(1, &"!!!!!!!!!!argument!!!!!!!!!!\n", 29);
 	while (a->arg[i])
 	{
 		write(1, &"[", 1);
@@ -32,18 +32,18 @@ void	show_arg(t_all *a)
 
 int		show_prompt(void)
 {
-    char    *prompt;
-    char    *temp;
+	char    *prompt;
+	char    *temp;
 	char	pwd[1024];
 
 	getcwd(pwd, 1024);
-    temp = ft_strjoin(PT_COLOR_LEFT, pwd);
-    prompt = ft_strjoin(temp, PT_COLOR_RIGHT);
-    free(temp);
-    temp = NULL;
-    write(1, prompt, ft_strlen(prompt));
-    write(1, PROMPT_MSG, ft_strlen(PROMPT_MSG));
-    free(prompt);
-    prompt = NULL;
+	temp = ft_strjoin(PT_COLOR_LEFT, pwd);
+	prompt = ft_strjoin(temp, PT_COLOR_RIGHT);
+	free(temp);
+	temp = NULL;
+	write(1, prompt, ft_strlen(prompt));
+	write(1, PROMPT_MSG, ft_strlen(PROMPT_MSG));
+	free(prompt);
+	prompt = NULL;
 	return (1);
 }
