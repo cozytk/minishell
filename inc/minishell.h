263570173e8bff8846de5c48efd66208e36c23e4
Taehoon Kim <taehkim@c1r5s1.42seoul.kr>
2020-12-11 21:07:24 +0900

    minishell.h normi #1

--- inc/minishell.h
+++ inc/minishell.h
@@ -11,14 +11,14 @@
 # include <sys/wait.h>
 # include <sys/stat.h>
 # include <sys/errno.h>
-#define PROMPT_MSG " 👉 "
-#define WELCOME_MSG " 환영합니다. :\n       __  ____       _      __         ____\n      /  |/  (_)___  (_)____/ /_  ___  / / /        ___ ___\n     / /|_/ / / __ \\/ / ___/ __ \\/ _ \\/ / /        | | |_  |\n    / /  / / / / / / (__  ) / / /  __/ / /         |_  |  _|\n   /_/  /_/_/_/ /_/_/____/_/ /_/\\___/_/_/            |_|___|.kr\n\n"
-#define PT_COLOR_LEFT "\e[42;1;37m"
-#define PT_COLOR_RIGHT "\e[0m"
+# define PROMPT_MSG " > "
+# define WELCOME_MSG " welcome. :\n       __  ____       _      __         ____\n      /  |/  (_)___  (_)____/ /_  ___  / / /        ___ ___\n     / /|_/ / / __ \\/ / ___/ __ \\/ _ \\/ / /        | | |_  |\n    / /  / / / / / / (__  ) / / /  __/ / /         |_  |  _|\n   /_/  /_/_/_/ /_/_/____/_/ /_/\\___/_/_/            |_|___|.kr\n\n"
+# define PT_COLOR_LEFT "\e[42;1;37m"
+# define PT_COLOR_RIGHT "\e[0m"
 
 # define INIT "bash-3.2$ "
 
-typedef struct		s_parse
+typedef struct	s_parse
 {
 	int		i;
 	int		start;
@@ -26,29 +26,29 @@ typedef struct		s_parse
 	int		s_colon;
 	int		parsing;
 	int		end;
-	char		*candidate;
-	char		*tmp;
-	char		*tmp2;	
-}					t_parse;
+	char	*candidate;
+	char	*tmp;
+	char	*tmp2;
+}				t_parse;
 
-typedef struct		s_all
+typedef struct	s_all
 {
 	char	**env;
 	char	**ept;
-	char 	**arg;
+	char	**arg;
 	char	**sub_env;
 	int		fd[2];
 	char	*cmd;
-	char 	*line;
+	char	*line;
 	char	*init_home;
 	int		fileno;
-	int 	redirect;
-	int 	fd_tmp;
-	int 	cd;
+	int		redirect;
+	int		fd_tmp;
+	int		cd;
 	t_parse	p;
-}					t_all;
+}				t_all;
 
-int		g_end;
+int			g_end;
 
 char		**check_identifier(char **mat);
 void		ft_erase(void);
@@ -61,11 +61,11 @@ int			cmd_exec(t_all *a);
 int			cmd_builtin(t_all *a);
 void		bash_cmd_error(char *cmd, char *msg, int exit_code);
 int			export(t_all *a);
-int 		cd(t_all *a);
+int			cd(t_all *a);
 int			echo(t_all *a);
 int			cmd_itself(char *cmd, char *str);
-int 		pwd(t_all *a);
-int		 	env(t_all *a);
+int			pwd(t_all *a);
+int			env(t_all *a);
 int			unset(t_all *a);
 char		**check_overlap(char **mat);
 char		**ft_delete_row(char **mat, int del);
@@ -81,10 +81,9 @@ int			ft_pipe(t_all *a);
 int			redirect(t_all *a);
 int			main_loop(t_all *a);
 int			ft_exit(t_all *a);
-int 		find_row(char **mat, char *s);
-int 		find_exact_row(char **mat, char *s);
-char	 	**ft_matadd_front(t_all *a);
-
+int			find_row(char **mat, char *s);
+int			find_exact_row(char **mat, char *s);
+char		**ft_matadd_front(t_all *a);
 
 /*
 **	parse functions (taehkim)
@@ -94,12 +93,12 @@ void		add_argument(t_all *a, char *arg);
 void		show_arg(t_all *a);
 void		show_com(t_all *a);
 void		free_com_arg(t_all *a);
-int     	parsing(t_all *a);
-int		is_sep_char(char c);
-int		chk_only_space(char *arg);
-int		is_space(char c);
-int		is_quote(char c);
-int		is_pipe_or_scolon(char c);
+int			parsing(t_all *a);
+int			is_sep_char(char c);
+int			chk_only_space(char *arg);
+int			is_space(char c);
+int			is_quote(char c);
+int			is_pipe_or_scolon(char c);
 char		*get_env_by_arg(t_all *a, char *arg, int *count);
 char		*get_env_by_arg2(t_all *a, char *arg, int *count, int i);
 char		*get_env_by_arg3(t_all *a, char *arg, int *count, int i);
@@ -109,14 +108,14 @@ void		s_quote_process(t_all *a);
 void		d_quote_process(t_all *a);
 void		quote_join(t_all *a);
 void		parse_init(t_all *a);
-int		parse_pipe_scolon(t_all *a);
+int			parse_pipe_scolon(t_all *a);
 void		parse_redirect(t_all *a);
 void		parse_quote(t_all *a);
 void		parse_one(t_all *a);
-int		parse_last(t_all *a);
+int			parse_last(t_all *a);
 void		add_candidate(t_all *a);
-int		validate(t_all *a);
-int		show_prompt(void);
+int			validate(t_all *a);
+int			show_prompt(void);
 void		equal_process(t_all *a);
 char		**add_row(char **mat, char *arg);
 int			same_key(char *s1, char *s2);
