332ca2c54e326adbb6f4a99b8279eabc94815d8c
taekkim <taekkim@student.42seoul.kr>
2020-12-11 19:41:11 +0900

    norm

--- srcs/chdir.c
+++ srcs/chdir.c
@@ -12,7 +12,7 @@
 
 #include "../inc/minishell.h"
 
-int 	write_cd_error(char *str, char *err, t_all *a, int exit)
+int		write_cd_error(char *str, char *err, t_all *a, int exit)
 {
 	ft_putstr_fd("bash: cd: ", 2);
 	ft_putstr_fd(str, 2);
@@ -36,11 +36,13 @@ int		cd_home(t_all *a, char flag)
 			return (1);
 		}
 		else if (flag == '~' && (chdir(a->init_home) == -1))
-				return (write_cd_error(a->init_home, ": No such file or directory", a, 127));
+				return (write_cd_error(a->init_home,
+						   ": No such file or directory", a, 127));
 		return (-1);
 	}
 	if (chdir(a->env[i] + 5) == -1)
-		return (write_cd_error(a->env[i] + 5, ": No such file or directory", a, 127));
+		return (write_cd_error(a->env[i] + 5,
+						 ": No such file or directory", a, 127));
 	else
 		g_end = 0;
 	return (0);
@@ -58,11 +60,12 @@ int		cd_oldpwd(t_all *a)
 		return (1);
 	}
 	if (chdir(a->env[i] + 7) == -1)
-		return (write_cd_error(a->env[i] + 7, ": No such file or direcotry", a, 127));
+		return (write_cd_error(a->env[i] + 7,
+						 ": No such file or direcotry", a, 127));
 	return (1);
 }
 
-int 	cd(t_all *a)
+int		cd(t_all *a)
 {
 	if (!(ft_strncmp(a->cmd, "cd", 2)))
 	{
@@ -74,7 +77,8 @@ int 	cd(t_all *a)
 		else if (ft_strlen(a->arg[0]) == 1 && !ft_strncmp(a->arg[0], "-", 1))
 			cd_oldpwd(a);
 		else if (chdir(a->arg[0]) == -1)
-			return (write_cd_error(a->arg[0], ": No such file or direcotry", a, 127));
+			return (write_cd_error(a->arg[0],
+						  ": No such file or direcotry", a, 127));
 		return (1);
 	}
 	return (0);
